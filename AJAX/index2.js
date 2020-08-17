0882cbcfdcf5e47aed914612e4be1e4d35c2efcb
akhil <akhilkhubchandani@gmail.com>
2020-08-17 10:52:31 +0530

    hidden frames  elective example

---
+++ AJAX/index2.js
@@ -0,0 +1,26 @@
+let obj = {
+	getElectives: function () {
+		let dep = document.getElementById("dept");
+		if (!dep.value) {
+			alert("Enter a department!");
+		} else {
+			let helper = document.getElementById("ifr");
+			helper.src = "hiddenFrames2.php?dept=" + dep.value;
+		}
+	},
+
+	populateElectives: function (electives) {
+		//the string returned by the server is ';' is seperated
+		//the 0th index is the dept name
+		let returnedArray = electives.split(";");
+		document.getElementById("dept").value = returnedArray[0];
+		//loop through electives
+		let sel = document.getElementById("el");
+		sel.innerHTML = "";
+		returnedArray.forEach((elective) => {
+			let option = document.createElement("option");
+			option.innerHTML = elective;
+			sel.appendChild(option);
+		});
+	},
+};
