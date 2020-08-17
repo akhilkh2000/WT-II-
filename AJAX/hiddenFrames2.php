adbb84906fcdb3e8f7b6628046cf82728d94142e
akhil <akhilkhubchandani@gmail.com>
2020-08-17 11:05:00 +0530

    added server side php code

---
+++ AJAX/hiddenFrames2.php
@@ -0,0 +1,24 @@
+<html>
+<script>
+    <?php
+    extract($_GET);
+    $file = fopen("electives.txt", "r");
+    //extracting line by line
+    while ($line = fgets($file)) {
+        $modline = trim($line); // remove extra spaces chars
+        $arr = explode(';', $modline); // as it is seperated by ;
+        $found = false;
+        if ($dept == $arr[0]) {
+            $found = true;
+            break;
+        }
+    }
+
+    if ($found == true) {
+        echo "parent.obj.populateElectives('$modline');";
+    }
+
+    ?>
+</script>
+
+</html>
