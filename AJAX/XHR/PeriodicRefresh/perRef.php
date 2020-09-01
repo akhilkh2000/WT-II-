d3fd321473a7bb901a81d3db30d71624feb657ac
akhil <akhilkhubchandani@gmail.com>
2020-09-01 10:18:15 +0530

    fallback for perRefresh

--- AJAX/XHR/PeriodicRefresh/perRef.php
+++ AJAX/XHR/PeriodicRefresh/perRef.php
@@ -1,13 +1,13 @@
  <?php
-    $file = fopen("score.txt", "w"); //to rewrite existing info as well
-    //generate random scores string
-    $update = rand(50, 100) . ";" . rand(100, 200) . ";" . rand(120, 240); //joining with ; as seperator
-    //write it to the file
-    fwrite($file, $update);
-    fclose($file);
-    $file = fopen("score.txt", "r");
-    //send file data back to client
-    sleep(5); //now  server will be bombarded with requests as every 3 seconds we do a periodic refresh and server 
-    // we have made server sleep for 5 seconds so now we need to implement a fallback plan when server is unresponsive
-    echo fread($file, filesize("score.txt"));
-    ?>
+   $file = fopen("score.txt", "w"); //to rewrite existing info as well
+   //generate random scores string
+   $update = rand(50, 100) . ";" . rand(100, 200) . ";" . rand(120, 240); //joining with ; as seperator
+   //write it to the file
+   fwrite($file, $update);
+   fclose($file);
+   $file = fopen("score.txt", "r");
+   //send file data back to client
+   sleep(5); //now  server will be bombarded with requests as every 3 seconds we do a periodic refresh and server 
+   // we have made server sleep for 5 seconds so now we need to implement a fallback plan when server is unresponsive
+   echo fread($file, filesize("score.txt"));
+   ?>
