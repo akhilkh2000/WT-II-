e560caa27b308896725e3d545e3d728da60377a3
akhil <akhilkhubchandani@gmail.com>
2020-09-23 10:57:19 +0530

    basic file ops

--- NodeJs/note/app.js
+++ NodeJs/note/app.js
@@ -1,7 +1,37 @@
 const fs = require("fs");
 const utils = require("../note/utils.js");
 
-const sum = utils.sum(3, 2);
-console.log(sum);
+// const sum = utils.sum(3, 2);
+// console.log(sum);
 
-fs.writeFileSync("notes.txt", "this was created by node.js!");
+fs.open("notes.txt", "r+", function (err, fd) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("opened file successfully");
+		var bufr = new Buffer.alloc(1024);
+		fs.ftruncate(fd, 10, function (err) {
+			//deletes content after 10th byte
+			if (err) {
+				console.log(err);
+			} else console.log("truncated file successfully");
+		});
+
+		fs.read(fd, bufr, 0, bufr.length, 0, function (err, bytes) {
+			if (err) {
+				console.log(err);
+			} else {
+				if (bytes > 0) console.log(bufr.slice(0, bytes).toString());
+			}
+		});
+		fs.close(fd, function (err) {
+			if (err) console.log(err);
+			else console.log("file closed successfully");
+		});
+	}
+});
+//fs.writeFileSync("notes.txt", "this was created by node.js!");
+fs.unlink("notes.txt", function (err) {
+	if (err) console.log(err);
+	else console.log("deleted file");
+});
--- NodeJs/note/utils.js
+++ NodeJs/note/utils.js
@@ -1,4 +1,4 @@
-console.log("utils.js file");
+// console.log("utils.js file");
 
 const add = function (a, b) {
 	return a + b;
