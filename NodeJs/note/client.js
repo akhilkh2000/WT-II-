1a33bfc7e01e542dc78763a588784b965c192b63
akhil <akhilkhubchandani@gmail.com>
2020-09-30 15:36:13 +0530

    client and server in nodejs

---
+++ NodeJs/note/client.js
@@ -0,0 +1,27 @@
+const http = require("http");
+//options for request header
+const options = {
+	host: "localhost",
+	port: "5000",
+	path: "./example.html",
+};
+
+//callback that processes the response
+var callback = function (res) {
+	console.log("in callback");
+	let body = "";
+	res.on("data", function (data) {
+		body += data;
+	});
+
+	res.on("end", function () {
+		console.log(body);
+	});
+};
+
+//make a request
+var req = http.request(options, callback);
+req.on("error", (e) => {
+	console.error(`problem with request: ${e.message}`);
+});
+req.end();
---
+++ NodeJs/note/server.js
@@ -0,0 +1,27 @@
+//http module creating a web server
+const http = require("http");
+const fs = require("fs");
+const url = require("url"); //to break url into parts and functions related
+
+//create server
+http
+	.createServer(function (req, res) {
+		var pathname = url.parse(req.url).pathname;
+		console.log("received request for pathname" + pathname);
+		fs.readFile(pathname.substr(1), (err, data) => {
+			if (err) {
+				console.log(err);
+				res.writeHead(404, { "Content-Type": "text/html" });
+			} else {
+				console.log("success");
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.write(data.toString()); // this is the response that will be displayed
+			}
+
+			//send to client and end the response
+			res.end();
+		});
+	})
+	.listen(5000);
+
+console.log("server is listening on port 5000");
