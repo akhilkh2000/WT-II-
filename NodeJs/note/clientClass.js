0c2413d6350448a932061a798dd2961ac52c71f3
akhil <akhilkhubchandani@gmail.com>
2020-10-01 19:02:22 +0530

    client post request using fetch

--- NodeJs/note/clientClass.js
+++ NodeJs/note/clientClass.js
@@ -3,8 +3,9 @@ var fetch = require("node-fetch");
 
 //fetch json file from server using GET
 fetch("http://localhost:5000/sample.json", {
-	method: "GET",
+	method: "POST",
+	body: JSON.stringify({ name: "modified value", col: "MIT" }),
 	headers: { "Content-Type": "application/json" },
 })
-	.then((res) => res.json())
+	//.then((res) => res.json())
 	.then((res) => console.log(res));
--- NodeJs/note/server.js
+++ NodeJs/note/server.js
@@ -8,19 +8,38 @@ http
 	.createServer(function (req, res) {
 		var pathname = url.parse(req.url).pathname;
 		console.log("received request for pathname" + pathname);
-		fs.readFile(pathname.substr(1), (err, data) => {
-			if (err) {
-				console.log(err);
-				res.writeHead(404, { "Content-Type": "text/html" });
-			} else {
-				console.log("success");
-				res.writeHead(200, { "Content-Type": "text/html" });
-				res.write(data.toString()); // this is the response that will be displayed
-			}
+		if (req.method == "GET") {
+			fs.readFile(pathname.substr(1), (err, data) => {
+				if (err) {
+					console.log(err);
+					res.writeHead(404, { "Content-Type": "text/html" });
+				} else {
+					console.log("success");
+					res.writeHead(200, { "Content-Type": "text/html" });
+					res.write(data.toString()); // this is the response that will be displayed
+				}
 
-			//send to client and end the response
-			res.end();
-		});
+				//send to client and end the response
+				res.end();
+			});
+		} else {
+			//request is a POST request
+			let body = [];
+			req
+				.on("data", (chunk) => {
+					body.push(chunk);
+				})
+				.on("end", () => {
+					//body has entire request body stored in it
+					//make it in string form
+					body = Buffer.concat(body).toString();
+				});
+			//write to JSON file
+			fs.writeFile(pathname.substr(1), body, (err, resp) => {
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.end();
+			});
+		}
 	})
 	.listen(5000);
 
