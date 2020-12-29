91fe282b8f18ab66305b74a1b9535e15df2a79cf
Akhil Khubchandani <56243241+akhilkh2000@users.noreply.github.com>
2020-12-29 13:54:11 +0530

    added CORS ability

--- AJAX/XHR/Blobresponse/xhrBlob.php
+++ AJAX/XHR/Blobresponse/xhrBlob.php
@@ -1,5 +1,6 @@
 <?php
 header("Content-type:video/mp4");
+header("Access-Control-Allow-Origin:*");
 
 $file = fopen("E:/PROJECTS/MATLAB/rec2.mp4", "rb");
 $f = fread($file, filesize("E:/PROJECTS/MATLAB/rec2.mp4"));
